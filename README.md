103294baaa9b3d69d57553e0cfd6bacd9b3bea83
navarral <74778335+navarral@users.noreply.github.com>
2021-12-07 08:35:36 +0000

    add apidata_fromcsv.py
    Signed-off-by: Albert Navarro Gallinad <albert.navarro@adaptcentre.ie>

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # serdif-api
+An API Wrapper to the [https://serdif-example.adaptcentre.ie/](https://serdif-example.adaptcentre.ie/).
+
+The goal of this wrapper is to facilitate the query process when associating environmental data
+with individual events through location and time.
+
+## How to use serdif-api
+
+### 1. Download serdif-api github repo
+[Download zip](https://github.com/navarral/serdif-api/archive/refs/heads/main.zip)
+or Clone the repo with `git clone https://github.com/navarral/serdif-api.git`
+
+### 2. Write a CSV file with the following structure:
+Environmental data will be queried within a region and from a time window (period) 
+prior to individual events as in the diagram before
+
+![image info](time-window_envoRecord.png)
+
+Therefore, we need to define the following parameters for each event:
+* **event**: event ID or name
+* **country**: standard two letter abbreviation for the country where the event happened
+* **region**: region(s) within the country (space separated)
+* **evDateT**: date of the event in a date time format
+* **wLen**: length of the time window in days
+* **wLag**: lag from the evDateT in days
+
+Example CSV input
+
+| event | country |    region     |       evDateT        | wLen | wLag |
+|:-----:|:-------:|:-------------:|:--------------------:|:----:|:----:|
+|   A   |   IE    |    WEXFORD    | 2018-02-05T00:00:00Z |  14  |  0   |
+|   B   |   IE    |    DUBLIN     | 2019-08-20T00:00:00Z |  14  |  7   |
+|   C   |   IE    |    WEXFORD    | 2020-11-01T00:00:00Z |  5   |  10  |
+|   D   |   IE    | CORK LIMERICK | 2021-04-30T00:00:00Z |  10  |  0   |
+
+### 3. Run the serdif apidata_fromcsv within the virtual environment
+Open a terminal in the project folder
+
+Activate the virtual environment: `source venv/bin/activate`
+
+Install the requirements: `pip install -r requirements.txt`
+
+Pass the following parameters to the apidata_fromcsv.py script
+* **path**: path to the CSV file
+* **timeUnit**: temporal units for retrieved environmental data set from: hour, day, month or year
+* **spAgg**: spatio-temporal aggregation method for the environmental data sets from: AVG, SUM, MIN or MAX
+* **dataFormat**: returning data format as 'CSV' or 'RDF'
+* **username**: username credentials for https://serdif-example.adaptcentre.ie/
+* **password**: password credentials for https://serdif-example.adaptcentre.ie/
+
+Check required parameters `python apidata_fromcsv.py -h`
+
+Example in-line command `python apidata_fromcsv.py event_data.csv day AVG CSV username password`
---
+++ apidata_fromcsv.py
@@ -0,0 +1,97 @@
+from apidata.api_data import serdifDataAPI
+import sys
+import pandas as pd
+from pprint import pprint
+import argparse
+import os
+
+# Create the parser
+serdif_apidata_parser = argparse.ArgumentParser(description='Query event-environmental linked data')
+
+# Add the arguments
+serdif_apidata_parser.add_argument(
+    'Path',
+    metavar='path',
+    type=str,
+    help='the path to the csv file')
+serdif_apidata_parser.add_argument(
+    'TimeUnit',
+    metavar='timeUnit',
+    type=str,
+    help='temporal units for retrieved environmental data set from: hour, day, month or year')
+serdif_apidata_parser.add_argument(
+    'SpatioTemporalAgg',
+    metavar='spAgg',
+    type=str,
+    help='spatio-temporal aggregation method for the environmental data sets from AVG, SUM, MIN or MAX')
+serdif_apidata_parser.add_argument(
+    'DataFormat',
+    metavar='dataFormat',
+    type=str,
+    help='returning data format as CSV or RDF')
+serdif_apidata_parser.add_argument(
+    'Username',
+    metavar='username',
+    type=str,
+    help='username credentials for https://serdif-example.adaptcentre.ie/')
+serdif_apidata_parser.add_argument(
+    'Password',
+    metavar='password',
+    type=str,
+    help='password credentials for https://serdif-example.adaptcentre.ie/')
+
+# Execute the parse_args() method
+args = serdif_apidata_parser.parse_args()
+print(args)
+
+# Check if user arguments are valid
+csvFile = args.Path
+if not os.path.isfile(csvFile):
+    print('The path specified does not exist\n')
+    sys.exit()
+if args.TimeUnit not in ['hour', 'day', 'month', 'year']:
+    print('The time unit specified is not hour, day, month or year\n')
+    sys.exit()
+if args.SpatioTemporalAgg not in ['AVG', 'SUM', 'MIN', 'MAX']:
+    print('The spatio-temporal aggregation method specified is not AVG, SUM, MIN or MAX\n')
+    sys.exit()
+if args.DataFormat not in ['CSV', 'RDF']:
+    print('The data format specified is not CSV or RDF\n')
+    sys.exit()
+if not args.Username:
+    print('The username argument is missing\n')
+    sys.exit()
+if not args.Password:
+    print('The password argument is missing\n')
+    sys.exit()
+
+# Import csv from command line
+print('Reading CSV file from input: ', csvFile, '\n')
+
+# Read region column as a list of regions
+df = pd.read_csv(csvFile, converters={'region': lambda x: x.split(' ')})
+print(df, '\n')
+
+# Format columns for serdif apidata
+print('Converting CSV to python dictionary for the serdif apidata ...\n')
+df['event'] = 'http://example.org/ns#event-' + df['event']
+df.index = df['event']
+event_dict = df.transpose().to_dict()
+pprint(event_dict)
+
+# Query the data
+print('\nSending query https://serdif-example.adaptcentre.ie/ ...\n')
+exampleData = serdifDataAPI(
+    evEnvoDict=event_dict,
+    # Select temporal units for the datasets used with environmental
+    # data from: 'hour', 'day', 'month' or 'year'
+    timeUnit=args.TimeUnit,
+    # Select spatiotemporal aggregation method for the datasets
+    # used with environmental data from: 'AVG', 'SUM', 'MIN' or 'MAX'
+    spAgg=args.SpatioTemporalAgg,
+    # Select the returning data format as 'CSV' or 'RDF'
+    dataFormat=args.DataFormat,
+    # Login credentials for https://serdif-example.adaptcentre.ie/
+    username=args.Username,
+    password=args.Password,
+)
